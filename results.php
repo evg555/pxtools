b3be8f9b905c7f49fafc286343d81ac19c1a512d
evg <ontropos@gmail.com>
2018-07-03 23:42:34 +1000

    Создан функционал получения данных

--- results.php
+++ results.php
@@ -1,3 +1,14 @@
+<?php
+    $post = $_POST['response'] ?? '';
+
+    if($post){
+        $userData = $post["user_data"] ?? [];
+        $topData = $post['top_data'] ?? [];
+    }
+?>
+
+
+
 <a class="return" href="/">Назад</a>
 
 <div class="inner">
@@ -21,19 +32,19 @@
             </tr>
             <tr>
                 <td>Объем текста в словах / символах</td>
-                <td>0 / 0 (849 / 7 378) </td>
-                <td>849 / 7 378</td>
+                <td><?=$userData['text_volume']['words']?> / <?=$userData['text_volume']['symbols']?> (849 / 7 378) </td>
+                <td><?=$topData['text_volume']['words']?> / <?=$topData['text_volume']['symbols']?> 378</td>
             </tr>
             <tr>
                 <td>Количество точных вхождений запроса в текст</td>
-                <td>0</td>
-                <td>0</td>
+                <td><?=$userData['text_volume']['query_in_text_exact']?></td>
+                <td><?=$topData['text_volume']['query_in_text_exact']?></td>
             </tr>
             <tr>
                 <td>Процент вхождения слов из запроса в текст</td>
                 <td>
-                    <p>купить: 0% (0.9%)</p>
-                    <p>canon: 0% (1.5%)</p>
+                    <p><?=$userData['text_volume']['query_words_in_text_percent']['word']?>: <?=$userData['text_volume']['query_words_in_text_percent']['percent']?>% (0.9%)</p>
+                    <p><?=$topData['text_volume']['query_words_in_text_percent']['word']?>: <?=$topData['text_volume']['query_words_in_text_percent']['percent']?>% (1.5%)</p>
                 </td>
                 <td>
                     <p>купить: 0% (0.9%)</p>
@@ -43,8 +54,8 @@
             <tr>
                 <td>Вхождения слов из запроса в тег Title</td>
                 <td>
-                    <p>купить: 0% (0.9%)</p>
-                    <p>canon: 0% (1.5%)</p>
+                    <p><?=$userData['text_volume']['query_words_in_title']['word']?>: <?=$userData['text_volume']['query_words_in_title']['count']?>% (0.9%)</p>
+                    <p><?=$topData['text_volume']['query_words_in_title']['word']?>: <?=$topData['text_volume']['query_words_in_title']['count']?>% (1.5%)</p>
                 </td>
                 <td>
                     <p>купить: 0% (0.9%)</p>
@@ -54,8 +65,8 @@
             <tr>
                 <td>Вхождения в анкоры исходящих ссылок</td>
                 <td>
-                    <p>купить: 0% (0.9%)</p>
-                    <p>canon: 0% (1.5%)</p>
+                    <p><?=$userData['text_volume']['query_words_in_anchors']['word']?>: <?=$userData['text_volume']['query_words_in_anchors']['count']?>% (0.9%)</p>
+                    <p><?=$topData['text_volume']['query_words_in_anchors']['word']?>: <?=$topData['text_volume']['query_words_in_anchors']['count']?>% (1.5%)</p>
                 </td>
                 <td>
                     <p>купить: 0% (0.9%)</p>
@@ -86,18 +97,30 @@
             </tr>
             <tr>
                 <td>Отсутствие в тексте слов, которые часто встречаются у конкурентов</td>
-                <td>canon, фотоаппарат, отзыв, купить, товар, магазин, цена, зеркальный, eos, размер, телефон, цифровой, характеристика, техника, выбирать</td>
-                <td></td>
+                <td>
+                    <?if($userData['text_volume']['popular_words']):?>
+                        <?foreach ($userData['text_volume']['popular_words'] as $popular_word):?>
+                            <?=$popular_word.", "?>
+                        <?endforeach;?>
+                    <?endif;?>
+                </td>
+                <td>
+	                <?if($topData['text_volume']['popular_words']):?>
+                        <?foreach ($topData['text_volume']['popular_words'] as $popular_word):?>
+                            <?=$popular_word.", "?>
+                        <?endforeach;?>
+	                <?endif;?>
+                </td>
             </tr>
             <tr>
                 <td>Число релевантных документов по тегу Title</td>
-                <td>86 </td>
-                <td>2</td>
+                <td><?=$userData['text_volume']['rel_docs_by_title']?></td>
+                <td><?=$topData['text_volume']['rel_docs_by_title']?></td>
             </tr>
             <tr>
                 <td>Длина тега Title в словах / символах</td>
-                <td>0 / 0 (13 / 103)</td>
-                <td>13 / 103</td>
+                <td><?=$userData['text_volume']['title_volume']['words']?> / <?=$userData['text_volume']['title_volume']['symbols']?> (13 / 103)</td>
+                <td><?=$topData['text_volume']['title_volume']['words']?> / <?=$topData['text_volume']['title_volume']['symbols']?></td>
             </tr>
             <tr>
                 <td>Внешних ссылок на домен</td>
@@ -106,18 +129,18 @@
             </tr>
             <tr>
                 <td>тИЦ</td>
-                <td>0 (1490)</td>
-                <td>1490</td>
+                <td><?=$userData['text_volume']['tcy']?> (1490)</td>
+                <td>1<?=$topData['text_volume']['tcy']?></td>
             </tr>
             <tr>
                 <td>Наличие HTTPS и доля конкурентов на HTTPS</td>
-                <td>Нет</td>
-                <td>80%</td>
+                <td><?($userData['text_volume']['https'] ? "Да" : "Нет")?></td>
+                <td><?($topData['text_volume']['https'] ? "Да" : "Нет")?></td>
             </tr>
             <tr>
                 <td>Наличие номера 8 800 и доля конкурентов c 8 800</td>
-                <td>Нет</td>
-                <td>40%</td>
+                <td><?echo ($userData['text_volume']['8800'] ? $userData['text_volume']['8800']: "Нет")?></td>
+                <td><?echo ($topData['text_volume']['8800'] ? $topData['text_volume']['8800'] : "Нет")?></td>
             </tr>
         </table>
 
